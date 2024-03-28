afd605a6c9cdc98f1ffb301016b1be368fa9d72c
Shea Cronin <shealcronin@gmail.com>
2024-03-28 15:14:48 -0400

    Handle FE validation with formik and yup libraries

--- client/src/components/AuthForm.tsx
+++ client/src/components/AuthForm.tsx
@@ -1,7 +1,7 @@
 import { Form, Button } from 'react-bootstrap';
-import { useState } from 'react';
 import { useAuth } from '../context';
-import type { FormEvent } from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 interface AuthFormProps {
   formTitle: string;
@@ -20,42 +20,76 @@ export const AuthForm = ({
   onSubmit,
   confirmPassword = false,
 }: AuthFormProps) => {
-  const [validated, setValidated] = useState(false);
-  const { error } = useAuth();
+  const validationSchema = yup.object({
+    username: yup.string().required('Username must not be empty.'),
+    password: yup.string().required('Password must not be empty.'),
+    confirmPassword: confirmPassword
+      ? yup
+          .string()
+          .required('Password confirmation must not be empty.')
+          .oneOf([yup.ref('password')], 'Passwords must match.')
+      : yup.string(),
+  });
+  const { handleSubmit, handleChange, values, touched, errors } = useFormik({
+    initialValues: { username: '', password: '', confirmPassword: '' },
+    validationSchema,
+    onSubmit: (values) => onSubmit(values),
+  });
+
+  const { error: authRequestError } = useAuth();
 
   return (
     <Form
       noValidate
-      validated={validated}
       onSubmit={handleSubmit}
       className="bg-dark text-light p-4 rounded"
     >
       <h2 className="h4">{formTitle}</h2>
-      {error && <p className="text-danger">{error}</p>}
+      {authRequestError && <p className="text-danger">{authRequestError}</p>}
       <Form.Group controlId="username" className="mb-3">
         <Form.Label>Username</Form.Label>
-        <Form.Control required placeholder="Enter username" />
+        <Form.Control
+          name="username"
+          type="text"
+          placeholder="Enter username"
+          value={values.username}
+          onChange={handleChange}
+          isValid={touched.username && !errors.username}
+          isInvalid={touched.username && !!errors.username}
+        />
         <Form.Control.Feedback type="invalid">
-          Username must not be empty.
+          {errors.username}
         </Form.Control.Feedback>
       </Form.Group>
       <Form.Group controlId="password" className="mb-3">
         <Form.Label>Password</Form.Label>
-        <Form.Control required type="password" placeholder="Password" />
+        <Form.Control
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={values.password}
+          onChange={handleChange}
+          isValid={touched.password && !errors.password}
+          isInvalid={touched.password && !!errors.password}
+        />
         <Form.Control.Feedback type="invalid">
-          Password must not be empty.
+          {errors.password}
         </Form.Control.Feedback>
       </Form.Group>
       {confirmPassword && (
         <Form.Group controlId="confirmPassword" className="mb-3">
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
-            required
+            name="confirmPassword"
             type="password"
             placeholder="Confirm Password"
+            value={values.confirmPassword}
+            onChange={handleChange}
+            isValid={touched.confirmPassword && !errors.confirmPassword}
+            isInvalid={touched.password && !!errors.confirmPassword}
           />
           <Form.Control.Feedback type="invalid">
-            Password must not be empty.
+            {errors.confirmPassword}
           </Form.Control.Feedback>
         </Form.Group>
       )}
@@ -64,24 +98,4 @@ export const AuthForm = ({
       </Button>
     </Form>
   );
-
-  function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-
-    const form = e.currentTarget;
-    const { username, password, confirmPassword } = form;
-
-    if (form.checkValidity() === false) {
-      e.stopPropagation();
-    } else {
-      const inputs = {
-        username: username.value,
-        password: password.value,
-        confirmPassword: confirmPassword?.value,
-      };
-      onSubmit(inputs);
-    }
-
-    setValidated(true);
-  }
 };
