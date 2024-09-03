df86a02d8e8004ce295bc20f5dbc1b00a8f3040c
Shea Cronin <shealcronin@gmail.com>
2024-09-03 10:53:01 -0400

    Update Program to start app so it can redirect

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+
 var app = builder.Build();
 
-app.Use(async (context, next) =>
-{
-    context.Response.Redirect("https://fancast.fly.dev/" + context.Request.Path
-        + context.Request.QueryString, permanent: true);
-    await next(context);
-});
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "api",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
